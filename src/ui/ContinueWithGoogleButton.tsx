d44380f1392c2c79682aa89f50296cf384fe8f11
DamianRavinduPeiris <drpeiris3@gmail.com>
2025-02-01 13:25:58 +0530

    Added Google Oauth.

--- src/ui/ContinueWithGoogleButton.tsx
+++ src/ui/ContinueWithGoogleButton.tsx
@@ -1,32 +1,18 @@
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 export default function ContinueWithGoogleButton() {
-    return (
-      <button
-        type="button"
-        className="flex w-100 items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-      >
-        <svg
-          className="mr-2 h-5 w-5"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          viewBox="0 0 48 48"
-        >
-          <defs>
-            <path
-              id="a"
-              d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
-            />
-          </defs>
-          <clipPath id="b">
-            <use xlinkHref="#a" overflow="visible" />
-          </clipPath>
-          <path clipPath="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
-          <path clipPath="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z" />
-          <path clipPath="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z" />
-          <path clipPath="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z" />
-        </svg>
-        Continue with Google
-      </button>
-    )
-  }
-  
-  
+  return (
+    <GoogleLogin
+      onSuccess={(credentialResponse) => {
+        console.log(
+          credentialResponse.credential
+            ? jwtDecode(credentialResponse.credential)
+            : "Credential is undefined"
+        );
+      }}
+      onError={() => {
+        console.log("Login Failed");
+      }}
+    />
+  );
+}
